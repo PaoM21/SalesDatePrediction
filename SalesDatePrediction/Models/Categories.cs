727da2af1fa6115ff786685cfd11b485d6a9f2db
PaoM21 <pao_951113@hotmail.com>
2024-10-02 19:03:44 -0500

    Creación de Models.

---
+++ SalesDatePrediction/Models/Categories.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDatePrediction.Models
+{
+    public class Categories
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ SalesDatePrediction/Models/Shippers.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDatePrediction.Models
+{
+    public class Shippers
+    {
+        public int ShipperId { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+    }
+}
