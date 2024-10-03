793c884d7b0e5d2fed241dd0a23034ee24998761
PaoM21 <pao_951113@hotmail.com>
2024-10-02 20:30:13 -0500

    Agregad model para OrderPrediction.

---
+++ SalesDatePrediction/Models/OrderPrediction.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDatePrediction.Models
+{
+    public class OrderPrediction
+    {
+        public string CompanyName { get; set; }
+        public DateTime LastOrderDate { get; set; }
+        public DateTime NextPredictedOrder { get; set; }
+    }
+}
